ec10384da94a80ee21f887c6c8085265b0a90599
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-08-19 22:14:37 +0530

    added box-shadow to mobile navbar

--- src/components/MobileNavbar/index.js
+++ src/components/MobileNavbar/index.js
@@ -6,6 +6,7 @@ const StyledMenu = styled.nav`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  box-shadow: -1px 0px 4px rgba(0, 0, 0, 0.05);
   background: #fff;
   transform: ${({ open }) => (open ? 'scaleX(100%)' : 'scaleX(0)')};
   height: 100vh;
