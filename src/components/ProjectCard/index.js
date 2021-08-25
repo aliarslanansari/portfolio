f3add3a942da608cb06e031d1b581dba6b849eda
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-08-25 21:25:45 +0530

    created project card

---
+++ src/components/ProjectCard/index.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import {
+  CustomProjectLink,
+  ProjectContainer,
+  ProjectDate,
+  ProjectDescription,
+  ProjectImage,
+  ProjectMetaContainer,
+  ProjectTitle,
+} from './style'
+import DevResProj from '../../assets/dev-resources-app.PNG'
+import Ellipsis from 'ant-design-pro/lib/Ellipsis'
+
+const ProjectCard = () => {
+  return (
+    <ProjectContainer>
+      <ProjectImage src={DevResProj} alt='React Portals' />
+      <ProjectMetaContainer>
+        <div>
+          <ProjectTitle
+            href={'https://blog.aliarslan.in/react-portals'}
+            target='_blank'
+            rel='noreferrer'>
+            {'React Portals '}
+          </ProjectTitle>
+          <ProjectDate>{`— ${'May 2021'}`}</ProjectDate>
+        </div>
+        <ProjectDescription>
+          <Ellipsis length={100}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            laborum beatae, consequatur ab quidem aperiam sapiente. Esse quas
+            incidunt rem.
+          </Ellipsis>
+        </ProjectDescription>
+        <CustomProjectLink>
+          <a
+            href={'https://blog.aliarslan.in/react-portals'}
+            rel='noreferrer'
+            target='_blank'>
+            Explore
+          </a>
+          <a
+            href={'https://blog.aliarslan.in/react-portals'}
+            rel='noreferrer'
+            target='_blank'>
+            Github
+          </a>
+          <a
+            href={'https://blog.aliarslan.in/react-portals'}
+            rel='noreferrer'
+            target='_blank'>
+            Live Demo
+          </a>
+        </CustomProjectLink>
+      </ProjectMetaContainer>
+    </ProjectContainer>
+  )
+}
+
+export default ProjectCard
---
+++ src/components/ProjectCard/style.js
@@ -0,0 +1,89 @@
+import styled from 'styled-components'
+import { getClickableLinkStyle } from '../../utils/styleUtils'
+
+export const ProjectTitle = styled.a`
+  margin: 0.5rem 0 0 0;
+  font-family: DM Serif Text;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1.5rem;
+  line-height: 1.5rem;
+  text-decoration-line: underline;
+  color: #3355ff;
+  &:hover {
+    color: #3355ff;
+  }
+`
+export const ProjectImage = styled.img`
+  width: 100%;
+  border-radius: 0.5rem;
+`
+export const ProjectMetaContainer = styled.div`
+  margin-top: -2rem;
+  border: 1px solid rgba(51, 85, 255, 0.3);
+  display: flex;
+  flex-direction: column;
+  padding: 0.1rem 0.5rem 0rem 0.5rem;
+  background: #f9f9fb;
+  border-radius: 0.5rem;
+`
+export const ProjectContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 0.5rem;
+`
+
+export const ProjectDate = styled.h4`
+  font-family: Segoe UI;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1rem;
+  display: inline-block;
+  color: #a1a1a2;
+  margin-left: 1rem;
+`
+
+export const ProjectDescription = styled.p`
+  font-family: Segoe UI;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1rem;
+  color: rgba(0, 0, 0, 0.87);
+`
+
+export const CustomProjectLink = styled.p`
+  align-self: flex-end;
+  font-family: Segoe UI;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1.1rem;
+  text-decoration: none;
+  cursor: pointer;
+  display: flex;
+  justify-content: space-around;
+  @media (max-width: 1200px) {
+    /* padding: 0 5rem 0 5rem; */
+    width: 50%;
+  }
+  @media (max-width: 1024px) {
+    /* padding: 0 3rem 0 3rem; */
+    width: 70%;
+  }
+  @media (min-width: 1201px) {
+    /* padding: 0 15rem 0 15rem; */
+    width: 60%;
+  }
+  @media (max-width: 520px) {
+    /* padding: 0 1.5rem 0 1.5rem; */
+    width: 80%;
+  }
+  & > a {
+    text-decoration: none;
+    color: #3355ff;
+    &:hover {
+      color: #3355ff;
+    }
+    ${getClickableLinkStyle()}
+  }
+`
