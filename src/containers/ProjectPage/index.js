8ae82f6ae4146cbec2d10870994cf30a7fe79275
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-09-01 14:17:00 +0900

    added neogcamp projects data and list on projects page

--- src/containers/ProjectPage/index.js
+++ src/containers/ProjectPage/index.js
@@ -1,10 +1,17 @@
+import Ellipsis from 'ant-design-pro/lib/Ellipsis'
 import { Col, Row } from 'antd/es/grid'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import PageWrapper from '../../components/PageWrapper'
 import ProjectCard from '../../components/ProjectCard'
-import { projectList } from '../../data'
+import {
+  CustomProjectLink,
+  ProjectDate,
+  ProjectDescription,
+  ProjectTitle,
+} from '../../components/ProjectCard/style'
+import { neogCampProjects, projectList } from '../../data'
 import { getClickableLinkStyle } from '../../utils/styleUtils'
 import { CustomTitle, StyledText, StyledTextName } from '../BlogsPage/styles'
 
@@ -18,6 +25,17 @@ const CustomRouterLink = styled(Link)`
     color: #3355ff;
   }
 `
+
+export const ProjectMetaContainer = styled.div`
+  border: 1px solid rgba(51, 85, 255, 0.3);
+  display: flex;
+  flex-direction: column;
+  padding: 0.1rem 0.5rem 0rem 0.5rem;
+  background: #f9f9fb;
+  border-radius: 0.5rem;
+  margin-bottom: 1rem;
+`
+
 const ProjectPage = ({ showAll }) => {
   const projects = showAll ? projectList : projectList.slice(0, 2)
   return (
@@ -54,6 +72,40 @@ const ProjectPage = ({ showAll }) => {
             <CustomRouterLink to='/project'>See more projects</CustomRouterLink>
           </Col>
         </Row>
+      )}
+      {showAll && (
+        <>
+          <CustomTitle>
+            <StyledTextName> Projects </StyledTextName>{' '}
+            <StyledText> for</StyledText>
+            <StyledTextName>neoG.Camp</StyledTextName>
+          </CustomTitle>
+
+          {neogCampProjects.map((project) => (
+            <ProjectMetaContainer>
+              <div style={{ marginTop: '0.5rem' }}>
+                <ProjectTitle
+                  href={project.code}
+                  target='_blank'
+                  rel='noreferrer'>
+                  {project.title}
+                </ProjectTitle>
+                <ProjectDate>{`— ${project.time}`}</ProjectDate>
+              </div>
+              <ProjectDescription>
+                <Ellipsis length={130}>{project.description}</Ellipsis>
+              </ProjectDescription>
+              <CustomProjectLink>
+                <a href={project.code} rel='noreferrer' target='_blank'>
+                  Github
+                </a>
+                <a href={project.link} rel='noreferrer' target='_blank'>
+                  Live Demo
+                </a>
+              </CustomProjectLink>
+            </ProjectMetaContainer>
+          ))}
+        </>
       )}
     </PageWrapper>
   )
